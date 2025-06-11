cc17fcd47d3df96b107da31a330a823796fb431f
Lodedx1419 <Khalid-alhaddad@outlook.com>
2025-06-11 23:31:29 +0300

    Update PacketTunnelProvider.swift

--- PacketTunnelExtension/PacketTunnelProvider.swift
+++ PacketTunnelExtension/PacketTunnelProvider.swift
@@ -1,1 +1,17 @@
+import NetworkExtension
 
+class PacketTunnelProvider: NEPacketTunnelProvider {
+
+    override func startTunnel(options: [String : NSObject]?, completionHandler: @escaping (Error?) -> Void) {
+        let settings = NEPacketTunnelNetworkSettings(tunnelRemoteAddress: "127.0.0.1")
+        settings.ipv4Settings = NEIPv4Settings(addresses: ["10.0.0.2"], subnetMasks: ["255.255.255.0"])
+        settings.dnsSettings = NEDNSSettings(servers: ["8.8.8.8", "1.1.1.1"])
+        setTunnelNetworkSettings(settings) { error in
+            completionHandler(error)
+        }
+    }
+
+    override func stopTunnel(with reason: NEProviderStopReason, completionHandler: @escaping () -> Void) {
+        completionHandler()
+    }
+}
